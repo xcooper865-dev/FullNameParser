d511b8202c5499994330d574a76fac9591500182
xaviercooper <xcooper@my.yearupunited.org>
2025-10-02 00:20:44 -0400

    full name

--- src/main/java/com/pluralsight/Fullname.java
+++ src/main/java/com/pluralsight/Fullname.java
@@ -1,9 +1,26 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Fullname {
     public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner (System.in);
+
+      System.out.println("First name here");
+String firstName = scanner.nextLine();
+
+System.out.println("Print last name ");
+String LastName = scanner.nextLine();
+
+String Fullname = firstName+""+LastName;
+System.out.println("First name:" +" " +firstName);
+System.out.println("last name:" +" "+LastName);
+
+
 
         }
     }
